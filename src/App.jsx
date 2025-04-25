39858c11b70f5e547e68efa048e2b322bfd64f7d
Cato Gjørven <cgj@uniconsult.no>
2025-04-25 21:11:39 +0200

    initial

--- src/App.jsx
+++ src/App.jsx
@@ -22,7 +22,7 @@ function App() {
           count is {count}
         </button>
         <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
+          Edit <code>src/App.jsx</code> and save to test HMR. Ding ding.
         </p>
       </div>
       <p className="read-the-docs">
