832d4fee2455c8c4ad46ae0280af4ea158cd8b9b
Cato Gjørven <cgj@uniconsult.no>
2025-04-26 12:19:42 +0200

    Minor fix

--- src/App.jsx
+++ src/App.jsx
@@ -22,7 +22,7 @@ function App() {
           count is {count}
         </button>
         <p>
-          Edit <code>src/App.jsx</code> and save to test HMR. Ding ding.
+          Testing <code>src/App.jsx</code> | Deploy to Cloudflare.
         </p>
       </div>
       <p className="read-the-docs">
