801bbff91f0cdf59786f092190c4b976cab942e5
Cato Gjørven <cgj@uniconsult.no>
2025-04-28 21:49:16 +0200

    Bootstrap nav

--- src/App.jsx
+++ src/App.jsx
@@ -1,39 +1,45 @@
+
 import { BrowserRouter as Router, Routes, Route, Link } from 'react-router-dom';
 import { Container, Navbar, Nav } from 'react-bootstrap';
 import MyCard from './MyCard';
+import BasicExample from './BasicNav';
+import './App.css'; // Make sure to import your CSS file
 
 function Home() {
-  return <h2>Welcome to the Home Page!</h2>;
+  return <h2>Welcome to the Home Page!</h2>;
 }
 
 function About() {
-  return <h2>This is the About Page.</h2>;
+  return <h2>This is the About Page.</h2>;
 }
 
 function App(props) {
-  return (
-    <div class="container">
-      <div class="row">
-        <div class="col">
-          <MyCard bgColor="bg-info" foreColor="text-white" />
-          <MyCard />
-          <MyCard />
-          <MyCard bgColor="bg-primary" foreColor="text-white" />
-          <MyCard />
-          <MyCard />
-        </div>
-        <div class="col">
-          <MyCard bgColor="bg-info" foreColor="text-white" />
-          <MyCard />
-          <MyCard />
-          <MyCard bgColor="bg-primary" foreColor="text-white" />
-          <MyCard />
-          <MyCard />
-        </div>
+  return (
+    <div className="container">
+        <div>
+        <BasicExample />
       </div>
+      <div className="row">
+        <div className="col-sm-9">
+            <MyCard className="" bgColor="bg-info" foreColor="text-white" />
+          </div>
+        <div className="col-sm-3">
+            <MyCard className="" bgColor="bg-info" foreColor="text-white" />
+          </div>
+<div className="col-sm-3">
+            <MyCard className="" bgColor="bg-info" foreColor="text-white" />
+          </div>
+<div className="col-sm-3">
+            <MyCard className="" bgColor="bg-info" foreColor="text-white" />
+          </div>
+<div className="col-sm-3">
+            <MyCard className="" bgColor="bg-info" foreColor="text-white" />
+          </div>
+       
+      </div>
 
-    </div>    
-  );
+    </div>    
+  );
 }
 
 export default App;
